2c2d5bc3842bd1f3bf8f1e1cce39cd396b2dc2f8
0tr1cala-v <marenkov2015@mail.ru>
2024-12-17 17:07:58 +0300

    Add files via upload

---
+++ UrbanDjango/task5/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from .forms import UserRegister
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def sign_up_by_django(request):
+    users = ['Django', 'Boba', 'Rex', 'Cody']
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = int(form.cleaned_data['age'])
+
+            if password != repeat_password:
+                info['error'] = "Пароли не совпадают"
+                return HttpResponse('Пароли не совпадают')
+            elif age < 18:
+                info['error'] = "Вы должны быть старше 18"
+                return HttpResponse('Вы должны быть старше 18')
+            elif username in users:
+                info['error'] = "Пользователь уже существует"
+                return HttpResponse('Пользователь уже существует')
+            else:
+                info['welcome_message'] = f"Приветствуем, {username}!"
+                users.append(username)
+            return HttpResponse(f"Приветствуем, {username}!")
+        else:
+            form = UserRegister()
+            info['message'] = form
+
+        return render(request, 'fifth_task/registration_page.html', info)
+
+
+def sign_up_by_html(request):
+    users = ['Django', 'Boba', 'Rex', 'Cody']
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = int(request.POST.get('age'))
+
+        if password != repeat_password:
+            info['error'] = "Пароли не совпадают"
+            return HttpResponse('Пароли не совпадают')
+        elif age < 18:
+            info['error'] = "Вы должны быть старше 18"
+            return HttpResponse('Вы должны быть старше 18')
+        elif username in users:
+            info['error'] = "Пользователь уже существует"
+            return HttpResponse('Пользователь уже существует')
+        else:
+            info['welcome_message'] = f"Приветствуем, {username}!"  # Приветственное сообщение
+            users.append(username)
+        return HttpResponse(f"Приветствуем, {username}!")
+
+    return render(request, 'fifth_task/registration_page.html', context=info)
